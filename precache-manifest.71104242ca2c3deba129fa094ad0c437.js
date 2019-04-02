045448bdc796af5efe3496380a707b10f1faba9f
SupremeSalvatore <milosposkuricakg@gmail.com>
2019-04-02 09:33:39 +0200

    Updates

---
+++ precache-manifest.71104242ca2c3deba129fa094ad0c437.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94fdb2d25ccbfa9fe6d7",
+    "url": "/react-lyrics-finder/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "94fdb2d25ccbfa9fe6d7",
+    "url": "/react-lyrics-finder/static/js/main.e72801a0.chunk.js"
+  },
+  {
+    "revision": "b620f7ebf27a3be15b83",
+    "url": "/react-lyrics-finder/static/js/runtime~main.f19eb417.js"
+  },
+  {
+    "revision": "a7fe90710b844d61a840",
+    "url": "/react-lyrics-finder/static/js/2.c7919dcf.chunk.js"
+  },
+  {
+    "revision": "05f1c6da499a8b1e271595e819b30198",
+    "url": "/react-lyrics-finder/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8197703523c83b40d00115add7ec4c7a.js"
+  "/react-lyrics-finder/precache-manifest.71104242ca2c3deba129fa094ad0c437.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-lyrics-finder/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
